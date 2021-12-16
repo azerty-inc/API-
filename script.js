a73e556003b21ddd0290fd086cb595026f72c57a
azerty-inc <95235082+azerty-inc@users.noreply.github.com>
2021-12-16 17:50:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 # API-
-var weeks = new Date(Date.now() + 12096e5);
+var weeks = new Date(Date.now() + 12096e5)
 const apiKey =`9a310b7d46fbc7e00fbc62646ecc790c`
 const requette = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=${weeks}&with_watch_monetization_types=flatrate`
 
 fetch(requette)
 .then (data => {
         if(data.ok){
-            return data.json();
+            return data.json() 
     
         }else{
-            let unElt = document.getElementById('erreur')
+            let unElt = document.getElementById('erreur') 
             unElt.innerHTML = "<p>Bonjour, il y a une erreur</p>"
         }
 })
