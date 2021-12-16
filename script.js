a7e71aab8ac07c1eab6283a7a74478d559ad7541
azerty-inc <95235082+azerty-inc@users.noreply.github.com>
2021-12-16 17:39:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 # API-
 var weeks = new Date(Date.now() + 12096e5);
-console.log(weeks)
 const apiKey =`9a310b7d46fbc7e00fbc62646ecc790c`
-const requette = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2021-12-27&with_watch_monetization_types=flatrate`
+const requette = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=FR&region=FR&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=${weeks}&with_watch_monetization_types=flatrate`
 
 fetch(requette)
 .then (data => {
@@ -10,13 +9,13 @@ fetch(requette)
             return data.json();
     
         }else{
-             console.log("erreur");
+            let unElt = document.getElementById('erreur')
+            unElt.innerHTML = "<p>Bonjour, il y a une erreur</p>"
         }
 })
 .then(data => 
     {
     let recup = data.results.forEach((item, i) => {
-        console.log(item);
     });
 })
 
